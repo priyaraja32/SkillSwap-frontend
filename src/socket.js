54dda8f2dd6d5be8d1fc7618097bc7a9f933c293
priyaraja32 <rpriyanka31032004@gmail.com>
2026-04-07 21:02:15 +0530

    update the file

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://skillswap-backend-v4s4.onrender.com", {
   withCredentials: true,
   transports: ["websocket"],
   autoConnect: true,
